9d1293dc9632af7286d09559f601c3c0c92c5f5e
salymbaeva17 <salymbaeva17@gmail.com>
2022-07-13 23:57:51 +0600

    init

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// const username = document.querySelector('.username')
+// const sort = document.querySelector('.sort')
+// const order = document.querySelector('.order')
+// const perPage = document.querySelector('.per-page')
+// const userImage = document.querySelector('.user-img')
+//
+// function onChange(value) {
+//     username.value = value.trim()
+// }
+// fetch(`https://api.github.com/users/${username.value}`)
+//     .then((res) => res.json())
+//     .then(data => {
+//         userImage.src = data.avatar_url
+//     })
